ee86d6df9acee1d98301c4c9b077855db55dfc83
samuvf <samuelvf9@hotmail.com>
2026-01-14 11:19:53 -0300

    feat: gravity update

--- Bird.lua
+++ Bird.lua
@@ -7,6 +7,14 @@ function Bird:init()
 
   self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
   self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+  self.dy = 0
+end
+
+local GRAVITY = 20
+function Bird:update(dt)
+  self.dy = self.dy + GRAVITY * dt
+  self.y = self.y + self.dy
 end
 
 function Bird:render()
