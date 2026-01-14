4b76aac73c17392051183445670fb9a500ba881f
samuvf <samuelvf9@hotmail.com>
2026-01-14 11:15:21 -0300

    feat: adds a Bird sprite to the game and renders it in the center of the screen

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 push = require 'push'
 
+Class = require 'class'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -16,6 +18,9 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413 -- similar to the begining of the image as to not be noticeable
+
+require 'Bird'
+local bird = Bird()
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -49,5 +54,8 @@ function love.draw()
   push:start()
     love.graphics.draw(background, -backgroundScroll, 0)
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    bird:render()
+
   push:finish()
 end
