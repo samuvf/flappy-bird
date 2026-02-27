997a4f9bb03be70eb287cc2e21bcc4f07822c8ba
samuvf <samuelvf9@hotmail.com>
2026-02-27 12:38:44 -0300

    refactor: gameover state draws medals according to points sccored during life

--- states/GameOverState.lua
+++ states/GameOverState.lua
@@ -1,5 +1,9 @@
 GameOverState = Class{__includes = BaseState}
 
+GOLD_MEDAL_IMAGE = love.graphics.newImage('goldMedal.png')
+SILVER_MEDAL_IMAGE = love.graphics.newImage('silverMedal.png')
+COOPER_MEDAL_IMAGE = love.graphics.newImage('cooperMedal.png')
+
 function GameOverState:enter(params)
   self.score = params.score
 end
@@ -15,5 +19,14 @@ function GameOverState:render()
   love.graphics.printf('Game Over', 0, 64, VIRTUAL_WIDTH, 'center')
   love.graphics.setFont(mediumFont)
   love.graphics.printf('Score: ' .. tostring(self.score), 0, 150, VIRTUAL_WIDTH, 'center')
+
+  if self.score >= 15 then
+    love.graphics.draw(GOLD_MEDAL_IMAGE, VIRTUAL_WIDTH/2 - (GOLD_MEDAL_IMAGE:getWidth()/2), 165)
+  elseif self.score >= 10 then
+    love.graphics.draw(SILVER_MEDAL_IMAGE, VIRTUAL_WIDTH/2 - (SILVER_MEDAL_IMAGE:getWidth()/2), 165)
+  elseif self.score >= 5 then
+    love.graphics.draw(COOPER_MEDAL_IMAGE, VIRTUAL_WIDTH/2 - (GOLD_MEDAL_IMAGE:getWidth()/2), 165)
+  end
+
   love.graphics.printf('Press Enter to Restart', 0, 200, VIRTUAL_WIDTH, 'center')
 end
